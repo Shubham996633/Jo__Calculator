b1b11c99a7b28d6baec521887c7e2563b86b330b
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-05 22:28:24 +0530

    update

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+const initApp = () => {
+    const currentValueElem = document.querySelector('.currentvalue')
+    const previousValueElem = document.querySelector('.previousvalue')
+    let itemArray = []
+    const equationArray = []
+    let newNumberFlag = false
+
+    const inputButtons = document.querySelectorAll('.number')
+    inputButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const newInput = event.target.textContent
+            if(newNumberFlag){
+                currentValueElem.value = newInput
+                newNumberFlag = false
+            }else{
+                currentValueElem.value = 
+                    currentValueElem.value == 0
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`
+            }
+        })
+    })
+
+    const opButtons = document.querySelectorAll('.operator')
+    opButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            if(newNumberFlag){
+                previousValueElem.textContent = ''
+                itemArray = []
+            }
+
+            const newOperator = event.target.textContent
+            const currentVal = currentValueElem.value
+
+            if(!itemArray.length && currentVal == 0) return
+
+            if(!itemArray.length){
+                itemArray.push(currentVal, newOperator)
+                previousValueElem.textContent = 
+                    `${currentVal}
+                    ${newOperator}`
+
+                return newNumberFlag = true
+            }
+
+            if(itemArray.length){
+                itemArray.push(currentVal)
+                const equationObj = {
+                    num1 : parseFloat(itemArray[0]),
+                    num2 : parseFloat(currentVal),
+                    op : itemArray[1]
+                }
+                equationArray.push(equationObj)
+                const equationString = `${equationObj['num1']}
+                                        ${equationObj['op']}
+                                        ${equationObj['num2']}`
+                const newValue = calculate(equationString, currentValueElem)
+            }
+        })
+    })
+
+
+    const clearButtons = document.querySelectorAll('.clear, .clearEntry')
+    clearButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            currentValueElem.value = 0
+            if(event.target.classList.contains('clear')){
+                previousValueElem.textContent = ''
+                itemArray = []
+            }
+        })
+    })
+
+    const deleteButton = document.querySelector('.delete')
+    deleteButton.addEventListener('click', () => {
+        currentValueElem.value = currentValueElem.value.slice(0, -1)
+    })
+
+    const signChangeButton = document.querySelector('.signChange')
+    signChangeButton.addEventListener('click', () => {
+        currentValueElem.value = parseFloat(currentValueElem.value) * -1
+    })
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
+
+const calculate = (equation, currentValueElem) => {
+const regex = /(^[*/=])|(\s)/
+}
+
+// 53:12
